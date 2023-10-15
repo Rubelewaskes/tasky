03134e646e2cd0a04950cd37ac8e13e26f118ebf
Rubelewaskes <131644269+Rubelewaskes@users.noreply.github.com>
2023-10-15 06:27:56 +0300

    Update URLS.js

--- frontend/api/URLS.js
+++ frontend/api/URLS.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export const api = 'http://26.181.201.144:5000/';
+export const api = 'http://localhost:5000/';
 export const Task = `${api}task`;
 export const Login = `${api}login`;
 export const Leader = `${api}leader`;
